26c62f09fa62f92d3f4e8b3308ebe810ec1ebbb5
Jetonii <70644726+Jetonii@users.noreply.github.com>
2022-04-23 10:31:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -64,29 +64,45 @@ fillRandom = function () {
 fillRandom()
 fillRandom();
 
-var startingX, startingY, movingX, movingY;
-function touchStart(evt) {
+let startingX, startingY, endingX, endingY;
+let moving = false;
+function touchstart(evt) {
     startingX = evt.touches[0].clientX;
     startingY = evt.touches[0].clientY;
 }
-function touchMove(evt) {
-    movingX = evt.touches[0].clientX;
-    movingY = evt.touches[0].clientY;
+function touchmove(evt) {
+    moving = true;
+    endingX = evt.touches[0].clientX;
+    endingY = evt.touches[0].clientY;
 }
-function touchEnd() {
-    if (startingX + 100 < movingX) {
-        rightPressed()
-    } else if (startingX - 100 > movingX) {
-        console.log('left');
-        leftPressed()
+function touchend() {
+    if (!moving || block) return;
+    let touchDirection;
+    if ( Math.abs(endingX - startingX) > Math.abs(endingY - startingY) ) {
+        if ( endingX > startingX ) touchDirection = "ArrowRight";
+        else touchDirection = "ArrowLeft";
+    } else {
+        if ( endingY > startingY ) touchDirection = "ArrowDown";
+        else touchDirection = "ArrowUp";
     }
+    handleInteraction(touchDirection);
+    moving = false;
+}
 
-    if (startingY + 100 < movingY) {
-        console.log('down');
-        downPressed()
-    } else if (startingY - 100 > movingY) {
-        console.log('up');
-        upPressed()
+function handleInteraction(dir){
+    switch(dir){
+        case 'ArrowRight':
+            rightPressed()
+            break
+        case 'ArrowLeft':
+            leftPressed()
+            break;
+        case 'ArrowDown':
+            downPressed()
+            break
+        case 'ArrowUp':
+            upPressed()
+            break
     }
 }
 document.addEventListener('keydown', (event) => {
